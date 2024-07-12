57a6ac5909e7a0d69ef428a94c405ad8f954c27a
alowe04 <albertolowe280@gmail.com>
2024-07-12 15:09:30 -0500

    Add files via upload

---
+++ operaciones/geometria.py
@@ -0,0 +1,14 @@
+# geometria.py
+
+def area_circulo(a):
+    return 3.14 * a **2
+
+def perimetro_circulo(radio):
+    return 2 * 3.14  * radio
+
+def area_rectangulo (a, b):
+    return a * b
+
+def perimetro_rectangulo(a, b):
+    return 2 * (a + b)
+
---
+++ operaciones/main.py
@@ -0,0 +1,7 @@
+import matematica as mat
+import geometria as geo
+
+print(mat.multiplicacion(10, 5))
+print(mat.division(10, 5))
+print(geo.area_rectangulo(10, 5))
+print(geo.perimetro_rectangulo(10, 5))
